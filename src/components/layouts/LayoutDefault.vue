2c10cc7e4c733f25732914d37e951f0893664ad2
couch Janus <couchjanus@gmail.com>
2020-06-15 14:13:00 +0300

    added layout

---
+++ src/components/layouts/LayoutDefault.vue
@@ -0,0 +1,84 @@
+<template>
+<div>
+
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Shopaholic</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <router-link to="/" class="nav-link">Home</router-link>
+          <router-link to="/about" class="nav-link">About</router-link>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <b-container fluid="lg">
+      <b-row>
+        <b-col sm="4">Filters</b-col>
+        <b-col sm="8">
+          <main class="main">
+            <slot />
+          </main>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container fluid="lg">
+    <b-row>
+      <b-col sm>
+        <footer class="footer">&copy; Awesome Company</footer>
+      </b-col>
+    </b-row>
+  </b-container>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: `LayoutDefault`,
+  data() {
+    return {
+      user: null,
+    };
+  },
+  created() {
+    // Simulate fetching user data.
+    setTimeout(() => {
+      this.user = { name: `John Doe` };
+    }, 2000);
+  },
+};
+</script>
+
+<style lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
+  <layout-default>
+
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
+  </layout-default>
 </template>
 
 <script>
 // @ is an alias to /src
+import LayoutDefault from '@/components/layouts/LayoutDefault.vue';
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
+  	LayoutDefault,
     HelloWorld
   }
 };
